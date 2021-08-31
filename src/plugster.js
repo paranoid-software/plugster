9078b8721fb1d7aaa937192d728a8be6803aaa9b
Pablo Pinargote <pablo.pinargote@outlook.com>
2021-08-31 09:48:04 -0500

    Major update and refactor
    - Multilanguage support
    - Implicit controller naming support
    - Dynamic view creation support
    - Delayed registry building and event subscriptions binding
    - Minor bug fixes
    - Unreferenced arguments using lodash cloneDeep

--- src/plugster.js
+++ src/plugster.js
@@ -1,12 +1,21 @@
 export default class Plugster {
 
-    constructor(outlets) {
+    constructor(outlets, controllerName) {
 
-        this.name = this.constructor.name;
+        Plugster.document = $(document);
+
+        this.dictionary = {};
+        this.name = controllerName || this.constructor.name;
 
         console.log(`${this.name} Controller Instantiated.`);
 
-        if (!Plugster.registry) Plugster.registry = {};
+        if (!Plugster.registry) {
+            Plugster.registry = {};
+            $('[data-controller-name]').map(function(index, val) {
+               Plugster.registry[$(val).data('controllerName').toLowerCase()] = undefined;
+            });
+        }
+
         Plugster.registry[this.name.toLowerCase()] = this;
 
         if (!Plugster.explicitSubscriptions) Plugster.explicitSubscriptions = {};
@@ -15,8 +24,47 @@ export default class Plugster {
         this._ = outlets;
         this.childTemplates = {};
         this.init();
-        
-    };
+
+        let allPlugstersRegistered = true;
+        Object.keys(Plugster.registry).map(function(key) {
+            if(!Plugster.registry[key]) allPlugstersRegistered = false;
+        });
+
+        if(allPlugstersRegistered && !window.plugsters) {
+            // Binding HTML declared subscriptions
+            Object.keys(Plugster.registry).map(function (plugsterKey) {
+                let plugster = Plugster.registry[plugsterKey];
+                let root = Plugster.registry[plugsterKey]._.root;
+                Object.keys(root.data()).map(function (rootDataKey) {
+                    let dashed = rootDataKey.replace(/[A-Z]/g, m => "-" + m.toLowerCase());
+                    let parts = dashed.split('-');
+                    if (parts.length !== 3) return null;
+                    if (parts[0] !== 'on') return null;
+                    let methodName = root.data(rootDataKey);
+                    if(!plugster[methodName]) throw new Error(`There is no method ${methodName} within the ${plugster.name} controller !!!`);
+                    let plugsterNameLowerCased = parts[1];
+                    let eventNameLowerCased = parts[2];
+                    if(!Plugster.registry[plugsterNameLowerCased]) throw new Error(`There is no ${plugsterNameLowerCased} controller !!!`);
+                    let pubMethodsLowerCased = Object.getOwnPropertyNames(Object.getPrototypeOf(Plugster.registry[plugsterNameLowerCased])).map(methodName => {
+                        return methodName.toLowerCase();
+                    });
+                    if(pubMethodsLowerCased.indexOf(eventNameLowerCased) < 0) throw new Error(`There is no ${eventNameLowerCased} event in ${plugsterNameLowerCased} controller !!!`);
+                    Plugster.htmlDeclaredSubscriptions[`${plugsterNameLowerCased}_${eventNameLowerCased}_${plugster.name.toLowerCase()}`] = {listener: plugster, methodName: methodName};
+                });
+            });
+            window.plugsters = Plugster.registry;
+        }
+    }
+
+    setDictionary(value) {
+        this.dictionary = value;
+    }
+
+    translateTo(lang, text) {
+        if(!this.dictionary[lang]) return text;
+        if(!this.dictionary[lang][text]) return text;
+        return this.dictionary[lang][text];
+    }
 
     toString() {
         return this.name;
@@ -64,12 +112,14 @@ export default class Plugster {
                 filteredOutlet = filteredOutlets[0];
             }
 
+            filteredOutlet.attr('id',`${self.name}_${key}`);
+
             if (filteredOutlet.data('child-templates')) {
                 filteredOutlet.buildListItem = function (withTemplateIndex, itemKey, jsonData, outletsSchema, itemClickCallback) {
                     if (!this.items) this.items = {};
                     if (!this.items[itemKey]) this.items[itemKey] = {};
                     this.append(self.childTemplates[`${key}_${withTemplateIndex}`]);
-                    let outlets = self.compileChildTemplate(this.children().last(), outletsSchema);
+                    let outlets = self.compileChildTemplate(key, this.children().length - 1, this.children().last(), outletsSchema);
                     if (outlets === null) return null;
                     outlets.root.attr('data-key', itemKey);
                     if(itemClickCallback)
@@ -81,6 +131,7 @@ export default class Plugster {
                     return this.items[itemKey].outlets;
                 };
                 filteredOutlet.count = function () {
+                    if (!this.items) return 0;
                     return Object.keys(this.items).length;
                 };
                 filteredOutlet.getData = function (key) {
@@ -88,6 +139,10 @@ export default class Plugster {
                 };
                 filteredOutlet.getOutlets = function (key) {
                     return this.items[key].outlets;
+                };
+                filteredOutlet.delete = function(key) {
+                    this.children(`[data-key='${key}']`).remove();
+                    delete this.items[key];
                 };
                 filteredOutlet.clear = function () {
                     this.items = undefined;
@@ -107,24 +162,6 @@ export default class Plugster {
 
         self._.root = root;
 
-        // Binding HTML declared subscriptions
-        Object.keys(root.data()).map(function (key) {
-            let dashed = key.replace(/[A-Z]/g, m => "-" + m.toLowerCase());
-            let parts = dashed.split('-');
-            if (parts.length !== 3) return null;
-            if (parts[0] !== 'on') return null;
-            let methodName = root.data(key);
-            if(!self[methodName]) throw new Error(`There is no method ${methodName} within the ${self.name} controller !!!`);
-            let plugsterNameLowerCased = parts[1];
-            let eventNameLowerCased = parts[2];
-            if(!Plugster.registry[plugsterNameLowerCased]) throw new Error(`There is no ${plugsterNameLowerCased} controller !!!`);
-            let pubMethodsLowerCased = Object.getOwnPropertyNames(Object.getPrototypeOf(Plugster.registry[plugsterNameLowerCased])).map(methodName => {
-                return methodName.toLowerCase();
-            });
-            if(pubMethodsLowerCased.indexOf(eventNameLowerCased) < 0) throw new Error(`There is no ${eventNameLowerCased} event in ${plugsterNameLowerCased} controller !!!`);
-            Plugster.htmlDeclaredSubscriptions[`${plugsterNameLowerCased}_${eventNameLowerCased}_${self.name.toLowerCase()}`] = {listener: self, methodName: methodName};
-        });
-
         console.log(`Outlets for ${self.name} Controller were binded successfuly !!!`);
 
         window.Promise.all(childTemplatesLoadPromises).then(function () {
@@ -142,32 +179,38 @@ export default class Plugster {
         });
     }
 
-    compileChildTemplate(template, outletsSchema) {
+    compileChildTemplate(parentOutletId, childIndex, template, outletsSchema) {
         // TODO: Validate schema compliance.
         if (!outletsSchema || Object.keys(outletsSchema).length === 0) return null;
         let outlets = {};
         outlets.root = template;
         $.map(template.find('[data-child-outlet-id]'), function (outlet) {
+            $(outlet).attr('id', `${parentOutletId}_${$(outlet).data('child-outlet-id')}_${childIndex}`);
             outlets[$(outlet).data('child-outlet-id')] = $(outlet);
         });
         return outlets;
     }
 
     registerEventSignature(name, data, callback) {
-        $(this).on(name, data, callback);
+        let unreferenced = _.cloneDeep(data);
+        $(this).on(name, unreferenced, callback);
     }
 
     dispatchEvent(name, args) {
-        this.broadcastExplicitSubscriptionsMessages(name, args);
-        this.broadcastHtmlDeclaredSubscriptionsMessages(name, args);
-        $(this).trigger(new $.Event(name, {args: args}));
+        let unreferenced = _.cloneDeep(args);
+        this.broadcastExplicitSubscriptionsMessages(name, unreferenced);
+        this.broadcastHtmlDeclaredSubscriptionsMessages(name, unreferenced);
+        $(this).trigger(new $.Event(name, {args: unreferenced}));
     }
 
     broadcastExplicitSubscriptionsMessages(name, args) {
-        let simpleKey = this.name.toLowerCase();
-        let compoundKey = `${this.name}_${name}`.toLowerCase();
-        Plugster.explicitSubscriptions[simpleKey]?.onNewMessage(this.name, name, args);
-        Plugster.explicitSubscriptions[compoundKey]?.onNewMessage(this.name, name, args);
+        let self = this;
+        let keyPrefix = `${this.name}_${name}`.toLowerCase();
+        Object.keys(Plugster.explicitSubscriptions).map(function(key) {
+            if(key.startsWith(keyPrefix)) {
+                Plugster.explicitSubscriptions[key]?.onNewMessage(self.name, name, args);
+            }
+        });
     }
 
     broadcastHtmlDeclaredSubscriptionsMessages(name, args) {
@@ -180,13 +223,20 @@ export default class Plugster {
         });
     }
 
-    // Explicit subscriptions enrollment
+    // Explicit subscriptions enrollment, you will need a default listener on the subscriber called onNewMessage
     listenTo(pubPlugster, event) {
-        if (!event) {
-            Plugster.explicitSubscriptions[pubPlugster.name.toLowerCase()] = this;
+/*        if (!event) {
+            console.log(1,`${pubPlugster.name}_${this.name}`)
+            Plugster.explicitSubscriptions[`${pubPlugster.name}_${this.name}`.toLowerCase()] = this;
             return;
-        }
-        Plugster.explicitSubscriptions[`${pubPlugster.name}_${event.name}`.toLowerCase()] = this;
+        }*/
+        Plugster.explicitSubscriptions[`${pubPlugster.name}_${event.name}_${this.name}`.toLowerCase()] = this;
+    }
+
+    static createView(controllerName, htmlTemplateFile, callback) {
+        $.get({url: htmlTemplateFile, cache: false}, function(html) {
+            callback(html.replace('[CONTROLLER_NAME]', controllerName));
+        });
     }
 
 }
